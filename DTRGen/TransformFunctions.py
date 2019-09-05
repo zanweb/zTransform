3b60a61454dde53dad4c825ac6796aecf3b60b7a
zanweb <zanweb@163.com>
2019-09-05 14:50:05 +0800

    2019/08/21：zSlitting模块基本功能--c檩图纸下部为o基准            zConvert模块--未修改

--- DTRGen/TransformFunctions.py
+++ DTRGen/TransformFunctions.py
@@ -294,8 +294,8 @@ def gen_butler_part_to_lysaght(nc_info):
                     # v_hole_y = profile_height / 2 - round(v_hole.y)
                     v_hole_y = -(profile_height / 2 - round(v_hole.y))
                 elif v_hole.reference == 'u':
-                    # v_hole_y = -(profile_height / 2 - round(v_hole.y))
-                    v_hole_y = profile_height / 2 - round(v_hole.y)
+                    v_hole_y = -(profile_height / 2 - round(v_hole.y))
+                    # v_hole_y = profile_height / 2 - round(v_hole.y)
                 elif v_hole.reference == 's':
                     # v_hole_y = round(v_hole.y)
                     # v_hole_y = profile_height / 2 - round(v_hole.y)
@@ -315,8 +315,8 @@ def gen_butler_part_to_lysaght(nc_info):
                     # h_hole_y = profile_height / 2 - round(h_hole.y)
                     h_hole_y = -(profile_height / 2 - round(h_hole.y))
                 elif h_hole.reference == 'u':
-                    # h_hole_y = -(profile_height / 2 - round(h_hole.y))
-                    h_hole_y = profile_height / 2 - round(h_hole.y)
+                    h_hole_y = -(profile_height / 2 - round(h_hole.y))
+                    # h_hole_y = profile_height / 2 - round(h_hole.y)
                 elif h_hole.reference == 's':
                     # h_hole_y = round(h_hole.y)
                     # h_hole_y = profile_height / 2 - round(h_hole.y)
